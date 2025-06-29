bf26f62759ad55dafce071c9c386088eb1a116fa
Sean-Cinac <s.cinac@students.htl-leonding.ac.at>
2025-06-29 23:52:00 +0200

    type and username validation

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
 public class Main {
 
-    final static String[] allowedParameters = {"-n", "-t"};
+    private final String[] allowedParameters = {"-n", "-t"};
+    private final String type = "-t";
+    private final String usernameInput = "-n";
+    private final String[] allowedTypes = {"prevusernames"};
+    private String username;
+    private String urlToApi;
 
     public static void main(String[] args) {
-        String urlToApi;
-        String username; 
+       
 
+        // if (args[i - 1].equals("-n")) {
+                     //   String[] argumentInput = {args[i - 1], args[i]}; //argument + the parameter
+                       // executeArgument(argumentInput);
+                   // }
 
     }
+
     public boolean validateSyntax(String[] args) { //checking if the user is starting the program with the correct options
         boolean parameterMode = false;
         for (int i = 0; i < args.length; i++) {
@@ -17,12 +26,17 @@ public class Main {
             }
             if (parameterMode) {
                 if (checkIfSyntaxExists(args[i])) { //Cannot use another argument in the space of a parameter to previous argument
+                    System.out.println("No Parameter for option " + args[i - 1] + " entered");
                     printHelp();
                     return false;
                 } else {
                     parameterMode = false;
-                    String[] argumentInput = {args[i - 1], args[i]}; //argument + the parameter
-                    executeArgument(argumentInput);
+                   if (args[i - 1].equals(type)) {
+                        if (!validateType(args[i])) {
+                            System.out.println("Wrong Type entered");
+                            return false;
+                        }
+                   }
                 }
             }
             else if (checkIfSyntaxExists(args[i])) {
@@ -32,6 +46,7 @@ public class Main {
         }
         return true;
     }
+
     public boolean checkIfSyntaxExists(String syntaxStatement) {
         for (int i = 0; i < allowedParameters.length; i++) {
             if (allowedParameters[i].equals(syntaxStatement)) {
@@ -40,9 +55,37 @@ public class Main {
         }
         return false;
     }
+
+    public boolean validateType(String typeInput) {
+        for (int i = 0; i < allowedTypes.length; i++) {
+            if (allowedTypes[i].equals(typeInput)) {
+                return true;
+            }
+        }
+        return false; 
+    }
+
     public void executeArgument(String[] argument) {
+        switch(argument[0]) {
+            case "-n": //username
+                setUsername(argument[1]);
+                break;
+            case "-t": //type
+                switch(argument[1]) {
+                    case "prevusernames":
+                        getAllUsernames();
+                }
+        }
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void getAllUsernames() {
 
     }
+
     public void printHelp() {
 
     }
